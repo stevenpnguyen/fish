bd829db8b900a51a8d2fda8fd729ff8fbf440b19
Steven Nguyen <stevenpnguyen@users.noreply.github.com>
2026-08-10 19:36:49 +1000

    Make fish swim and randomize catch rewards
    Fish now swim continuously across the screen and can be caught
    for random reward amounts. The trophy for reaching 5000 is only
    awarded once, and the fox can now move down with the keyboard.

--- fish.py
+++ fish.py
@@ -8,13 +8,12 @@ WIDTH = 1750
 HEIGHT = 1000
 CENTRE_X = WIDTH / 2
 CENTRE_Y = HEIGHT / 2
-# screen = pygame.display.set_mode((WIDTH, HEIGHT))
-money = 10
+money = 0
 clicked = False
 message = "You have unlocked this achievement"
-
 show_trophy = False
 bg = pygame.transform.scale(pygame.image.load("images/ocean.png").convert(), (WIDTH, HEIGHT))
+first_trophy = False
 fox = Actor("penguin")
 fox.pos = (875, 300)
 fish = Actor("fish")
@@ -33,24 +32,25 @@ def draw():
         fish.draw()
     if fish2.x != -1000:
         fish2.draw()
-    screen.draw.text("money:  " + str(money), color="black", bottomleft=(10, 990))
+    screen.draw.text("money:  " + str(money), color="green", bottomleft=(10, 990))
     if clicked:
         screen.draw.text(message, (300, 10), fontsize=30, color="black")
     if show_trophy == True:
-        trophy_x = 950;
+        trophy_x = 1700;
         for no in range(trophy_no):
-    # Code to execute for each item
             trophy.draw()
             trophy.pos = (trophy_x, 50)
             trophy_x = trophy_x - 100
-
-
 
 def update():
     if keyboard.right:
         fox.x += 5
     elif keyboard.left:
         fox.x -= 5
+    elif keyboard.down:
+        fox.y += 5
+    fish.x -= 1
+    fish2.x -= 2
 
 def hide_fish():
     fish.x = -1000
@@ -60,7 +60,6 @@ def hide_fish2():
     fish2.x = -1000
     clock.schedule(show_fish2, 2.0)
 
-
 def show_fish(): # Reset the image name to show it
     fish.pos = (random.randint(0, 1750), random.randint(500, 1000))
     clock.schedule(hide_fish, 5.0)
@@ -68,9 +67,6 @@ def show_fish(): # Reset the image name to show it
 def show_fish2():
     fish2.pos = (random.randint(0, 1750), random.randint(500, 1000))
     clock.schedule(hide_fish2, 3.0)
-
-
-
 
 def hide_text():
     global clicked
@@ -80,6 +76,8 @@ def add_trophy():
     global trophy_no
     trophy_no = trophy_no + 1
 
+def hide_trophy():
+    show_trophy = False
 
 def on_mouse_down(pos):
     global money
@@ -87,14 +85,17 @@ def on_mouse_down(pos):
     global clicked
     global show_trophy
     global trophy_no
+    global first_trophy
     if fish2.collidepoint(pos):
-        money = money + 250
+        money = money + (random.randint(50, 500))
         fish2.pos = (random.randint(10, 1740), random.randint(340, 990))
         if money >= 5000:
-            clicked = True
-            clock.schedule(hide_text, 3.0)
-            add_trophy()
-            show_trophy = True
+            if not first_trophy:
+                clicked = True
+                clock.schedule(hide_text, 3.0)
+                add_trophy()
+                first_trophy = True
+                show_trophy = True
         if money >= 50000:
             clicked = True
             clock.schedule(hide_text, 3.0)
@@ -107,27 +108,25 @@ def on_mouse_down(pos):
             clicked = True
             clock.schedule(hide_text, 3.0)
             add_trophy()
-
     if fish.collidepoint(pos):
-        money = money + 100
+        money = money + (random.randint(25, 350))
         fish.pos = (random.randint(10, 1740), random.randint(340, 990))
         if money >= 5000:
             clicked = True
             clock.schedule(hide_text, 3.0)
             add_trophy()
+            first_trophy = True
             show_trophy = True
         if money >= 50000:
             clicked = True
             clock.schedule(hide_text, 3.0)
-            add_trophy()
         if money >= 500000:
             clicked = True
             clock.schedule(hide_text, 3.0)
-            add_trophy()
         if money >= 1000000:
             clicked = True
             clock.schedule(hide_text, 3.0)
-            add_trophy()
+
 clock.schedule(hide_fish, 5.0)
 clock.schedule(hide_fish2, 3.0)
 
