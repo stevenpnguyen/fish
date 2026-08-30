86c6a6b949a4c1f2f62148b247aac3cc1d776507
stevenpnguyen <stevenpnguyen0710@gmail.com>
2026-08-30 20:58:13 +1000

    Change dir to bundle dir before running launcher
    The bundled files are extracted to sys._MEIPASS, but pygame.image.load() and Actor(...) resolve relative paths against the working directory

--- executable/launcher.py
+++ executable/launcher.py
@@ -4,6 +4,7 @@ import sys
 BASE_DIR = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
 GAME_PATH = os.path.join(BASE_DIR, "fish.py")
 
+os.chdir(BASE_DIR)
 sys.argv = [sys.argv[0], GAME_PATH]
 
 from pgzero.runner import main
